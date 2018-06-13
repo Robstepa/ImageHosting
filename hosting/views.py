5234f9f22df9ad07172d0f79c47ca6a116f62761
Robstepa <zb.arthur@gmail.com>
2018-06-13 12:44:26 +0200

    Fix issues with saving file

--- hosting/views.py
+++ hosting/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 
+from ImageHosting import settings
+from hosting.models import Photo
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.jinja2')
 
 
 def gallery(request):
-    gallery = [1, 2, 3]
-    return render(request, 'gallery.jinja2', {'gallery': gallery})
+    photos = Photo.objects.all()
+    return render(request, 'gallery.jinja2', {'photos': photos, 'media_url': settings.MEDIA_URL})
 
 
 def upload(request):
-    return render(request, 'upload.jinja2')
+    info = ''
+    if request.method == 'POST':
+        Photo().image.save('img.jpg', request.FILES['photo'], True)
+        info = 'Done'
+    return render(request, 'upload.jinja2', {'info': info})
